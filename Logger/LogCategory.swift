bea7126ace082bb13092a64da69e28544f0efe2d
Sudara Fernando <sudara@yahoo.com>
2020-05-29 00:06:51 +1200

    Added Default Log Category

--- Logger/LogCategory.swift
+++ Logger/LogCategory.swift
@@ -7,6 +7,28 @@
 
 import Foundation
 
+#if canImport(SwiftUI)
+import SwiftUI
+#endif
+
 public protocol LogCategory: Equatable {
 	var category: String
+}
+
+public enum DefaultCategory: LogCategory {
+	case `default`
+
+	#if canImport(SwiftUI)
+	case ui(view: View.Type)
+	#endif
+
+	var category: String {
+		switch self {
+		case .default: return "default"
+
+		#if canImport(SwiftUI)
+		case .ui(let view): return "ui-\(view)"
+		#endif
+		}
+	}
 }
